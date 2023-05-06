b42c7914c17259e1ccf5c6f9da4fc19e57de125e
yasirarism <55983182+yasirarism@users.noreply.github.com>
2023-05-06 21:20:38 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,6 @@
 const { getScreenshot, getContent } = require("./_lib/puppeteer");
-const app = require("express")();
 
-app.get("/webss", async (req, res) => {
+module.exports = async function (req, res) {
   if (!req.query.url) return res.status(400).send("No url query specified.");
   if (!checkUrl(req.query.url)) return res.status(400).send("Invalid url query specified.");
   try {
@@ -9,20 +8,6 @@ app.get("/webss", async (req, res) => {
     res.setHeader("Content-Type", "image/png");
     res.setHeader("Cache-Control", "public, immutable, no-transform, s-maxage=86400, max-age=86400");
     res.status(200).end(file);
-  } catch (error) {
-    console.error(error)
-    res.status(500).send("The server encountered an error. You may have inputted an invalid query.");
-  }
-}
-
-app.get("/pahe", async (req, res) => {
-  if (!req.query.url) return res.status(400).send("No url query specified.");
-  if (!checkUrl(req.query.url)) return res.status(400).send("Invalid url query specified.");
-  try {
-    const content = await getContent(req.query.url);
-    res.setHeader("Content-Type", "text/html");
-    res.setHeader("Cache-Control", "public, immutable, no-transform, s-maxage=86400, max-age=86400");
-    res.status(200).end(content);
   } catch (error) {
     console.error(error)
     res.status(500).send("The server encountered an error. You may have inputted an invalid query.");
@@ -38,5 +23,3 @@ function checkUrl(string, hostname) {
   }
   return true;
 }
-
-module.exports = app;
